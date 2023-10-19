388f7364c4b36882ad6162f4f2504e3dfdef76ca
John Risoule <zeieleinopensource@gmail.com>
2023-10-20 01:55:54 +0800

    Added multimonitor fixes for polybar and i3.

---
+++ polybar/multibar.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+# If all your bars have ipc enabled, you can use 
+polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+# killall -q polybar
+
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload mybar &
+done
+
+echo "Bars launched..."
